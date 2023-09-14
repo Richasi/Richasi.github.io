65990fd43273e852bd2e6d8e599355913510b8da
Richa Singh <121396620+Richasi@users.noreply.github.com>
2023-09-14 13:58:06 +0530

    ll

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,39 +90,6 @@ modalCloses.forEach((modalCloses) => {
     });
 });
 
-/*==================== PORTFOLIO SWIPER  ====================*/
-// let swiperPortfolio = new Swiper(".portfolio__container", {
-//     cssMode: true,
-//     loop: true,
-
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//     },
-// });
-
-// /*==================== TESTIMONIAL ====================*/
-// let swiperTestimonial = new Swiper(".testimonial__container", {
-//     loop: true,
-//     grabCursor: true,
-//     spaceBetween: 48,
-
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//         dynamicBullets: true,
-//     },
-
-//     breakpoints: {
-//         568: {
-//             slidesPerView: 2,
-//         },
-//     },
-// });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section[id]");
